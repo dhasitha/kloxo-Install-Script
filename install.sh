455a629a042b34c3644fa810591fa8d6b4cc7687
Hasitha Dayarathna <hasithaya@gmail.com>
2017-03-26 18:04:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ cd /
 yum update -y
 yum install yum-utils yum-priorities vim-minimal subversion curl zip unzip -y
 yum install telnet wget -y
+setenforce 0
 cd /tmp
 rm -f mratwork*
-rpm -ivh https://github.com/mustafaramadhan/rpms/raw/master/mratwork/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm
+rpm -ivh https://github.com/mustafaramadhan/rpms/raw/master/mratwork/release/neutral/noarch/mratwork-release-0.0.1-1.noarch.rpm --no-check-certificate
 cd /
 yum clean all
 yum update mratwork-* -y
